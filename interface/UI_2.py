acdf61cfb8e6e2758dee56e0c0c9fc6e280c94f8
Иван Литвинов <litvan007@litvan.local>
2023-05-29 22:24:00 +0300

    improved UI 2

--- interface/UI_2.py
+++ interface/UI_2.py
@@ -55,21 +55,21 @@ class PSO_interface(QMainWindow): # главное окно
         self.resize(1200, 680) # размер окна
 
         # создаем фигуру и холст для графика
-        self.figure = plt.figure(figsize=(40, 40))
+        self.figure = plt.figure(figsize=(100, 100))
         self.canvas = FigureCanvas(self.figure)
         # создаем панель инструментов для управления графиком
-        self.toolbar = NavigationToolbar(self.canvas, self)
+        self.toolbar1 = NavigationToolbar(self.canvas, self)
         # добавляем холст и панель в центральный виджет
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
         self.layout1 = QVBoxLayout()
-        self.layout1.addWidget(self.toolbar)
+        self.layout1.addWidget(self.toolbar1)
         self.layout1.addWidget(self.canvas)
 
-        # создаем кнопки для анимации и конечного положения графика на одной горизонтальной линии
-        self.btn_animate = QPushButton("Анимация", self)
+        # # создаем кнопки для анимации и конечного положения графика на одной горизонтальной линии
+        # self.btn_animate = QPushButton("Анимация", self)
         # self.btn_animate.clicked.connect(self.show_animation)
-        self.btn_final = QPushButton("Конечная точка", self)
+        self.btn_final = QPushButton("Минимум", self)
         self.btn_final.clicked.connect(self.relevant_point_plot)
         
         # создаем горизонтальный лэйаут для кнопок
@@ -80,23 +80,27 @@ class PSO_interface(QMainWindow): # главное окно
         self.spinbox.valueChanged.connect(self.main_plot)
 
         self.label = QLabel(self)
-        self.label.setFrameStyle(QFrame.Panel | QFrame.Sunken)
-        self.label.setText(f" / {self.MAX_ITERATION}")
+        # self.label.setFrameStyle(QFrame.Panel | QFrame.Sunken)
+        self.label.setText(f"/ {self.MAX_ITERATION}")
+
+        self.label_point = QLabel(self)
+        self.label_point.setFrameStyle(QFrame.Panel | QFrame.Sunken)
+
+        self.label_fit = QLabel(self)
+        self.label_fit.setFrameStyle(QFrame.Panel | QFrame.Sunken)
+        
 
         btn_layout = QHBoxLayout()
-        btn_layout.addWidget(self.btn_animate)
+        # btn_layout.addWidget(self.btn_animate)
         btn_layout.addWidget(self.btn_final)
         btn_layout.addWidget(self.spinbox)
         btn_layout.addWidget(self.label)
+        btn_layout.addWidget(self.label_point)
+        btn_layout.addWidget(self.label_fit)
 
         # добавляем лэйаут с кнопками в основной лэйаут
         btn_layout.setAlignment(Qt.AlignCenter)
         self.layout1.addLayout(btn_layout)
-
-        # self.gif_label = QLabel(self)
-        # self.gif_label.setFixedSize(900, 600)
-        # self.layout2 = QVBoxLayout()
-        # self.layout2.addWidget(self.gif_label)
 
         # устанавливаем лэйаут в центральный виджет
         self.central_widget.setLayout(self.layout1)
@@ -105,12 +109,10 @@ class PSO_interface(QMainWindow): # главное окно
         PSO = Common(self.X, self.Y, self.Z, self.MIN_X, self.MAX_X, self.SPACE_DIMENSION, self.func, self.W,
                         self.C1, self.C2, self.MAX_ITERATION, self.PARTICLE_COUNT)
         self.position_history, self.best_swarm_x, self.best_swarm_y, self.best_swarm_fitness = PSO.run()
+        self.label_point.setText(f"Лучшее решение: X = {self.best_swarm_x}, Y = {self.best_swarm_y}")
+        self.label_fit.setText(f"Значение ф-ии: {self.best_swarm_fitness}")
         self.main_plot()
 
-        # self.gif_movie = QMovie("./PSO_animation.gif")
-        # self.gif_label.setMovie(self.gif_movie)
-
-    
     def main_plot(self):
         self.figure.clear()
         ax = self.figure.add_subplot(111, projection='3d')
@@ -137,27 +139,5 @@ class PSO_interface(QMainWindow): # главное окно
         ax.set_xlim(self.MIN_X, self.MAX_X)
         ax.set_ylim(self.MIN_X, self.MAX_X)
         ax.set_zlim(0, 100)
-        ax.plot(self.best_swarm_x, self.best_swarm_y, self.best_swarm_fitness, 'g.')
+        ax.plot(self.best_swarm_x, self.best_swarm_y, self.best_swarm_fitness, 'g.', marker='o', markersize=7,)
         self.canvas.draw()
-
-    def show_animation(self):
-        current_layout = self.central_widget.layout()
-        widget = current_layout.takeAt(1).widget()
-        widget.deleteLater()
-
-        if current_layout == self.layout1:
-            self.central_widget.setLayout(self.layout1)
-        #     # меняем текст кнопки
-        else:
-        #     # если был лэйаут с графиком, то переключаемся на лэйаут с гифкой
-            self.gif_movie.start()
-            self.gif_label.setScaledContents(True)
-            self.central_widget.setLayout(self.layout2)
-        #     # меняем текст кнопки
-
-
-
-
-
-
-
